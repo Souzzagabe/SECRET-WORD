ac24fb0beca773f3363059d3233f3e76497628da
Gabriel <souzzagabe@hotmail.com>
2023-11-20 18:11:55 -0300

    ajustes

--- secret-word/src/App.jsx
+++ secret-word/src/App.jsx
@@ -8,14 +8,43 @@ import GameOver from './components/GameOver'
 const stages = [
   {id: 1, name: "start"},
   {id: 2, name: "game"},
-  {id: 3, name: "end"},
+  {id: 3, name: "end"}
 ]
 
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
 
+  const [pickedWord, setPickedWord] = useState("")
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState([])
+
+  const pickWordAndCategory = () => {
+    const categories = Object.keys(words)
+    const category = 
+    categories[Math.floor(Math.random() * Object.keys(categories).length)]
+
+    const word = 
+    words[category][Math.floor(Math.random() * words[category].length)]
+    console.log(word)
+
+    return {word, category}
+  }
+
   const startGame = () => {
+    const { word, category } = pickWordAndCategory()
+
+    let wordLetters = word.split("")
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase())
+
+    console.log(word, category)
+    console.log(wordLetters)
+
+    setPickedCategory(category)
+    setPickedWord(word)
+    setLetters(letters)
+
     setGameStage(stages[1].name)
   }
 
